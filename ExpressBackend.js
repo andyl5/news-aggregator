b30cf53662a5f8991fcc34ae8899c891fbcad4a3
andy <andymac@andys-iMac.local>
2023-05-27 01:20:18 -0400

    backend notes

--- ExpressBackend.js
+++ ExpressBackend.js
@@ -1,11 +1,51 @@
+import { useLocation } from 'react-router';
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3001;
 
+const axios = require('axios');
+const { JSDOM } = require('jsdom');
+const { Readability } = require('@mozilla/readability');
+
+
 app.use(cors()); // Enable CORS for all routes
 
+
+// TODO
+
+// TRY THIS APPROACH
+// oh and it needs to be able to take in the url from the frontend. try the 2nd approach, putting it in the Request Body
+// frontend portion
+// const articleUrl = "https://example.com/article";
+// fetch('/api', {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify({ url: articleUrl })
+// })
+//   .then((res) => res.json())
+//   .then((data) => {
+//     // Handle response
+//   });
+
+// backend portion
+// app.use(express.json()); // Middleware to parse request body
+
+// app.post('/api', (req, res) => {
+//   const articleUrl = req.body.url;
+//   // Process the article URL and fetch the content
+//   // ...
+// });
+
+
+
+// add the article scrape logic. it should display the article content on the read route
+// possible solution below
+
 app.get('/api', (req, res) => {
+  // axios get request to article url, then the code from the snippet, then set that articlecontent which comes from (usestate) like this, content: articleContent
+
   res.json({ content: "test yooo kinda working" });
 });
 
